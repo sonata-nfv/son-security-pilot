637a2fcbda86679d6a770187dca3f7add7a9f6b2
cgeoffroy <cgeoffroy@users.noreply.github.com>
2017-10-10 15:35:53 +0200

    Fix invalide entrypoint in setup.py

--- fsm/firewall-config/setup.py
+++ fsm/firewall-config/setup.py
@@ -70,6 +70,6 @@ setup(
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
-        'console_scripts': ['pfsense-config=pfsense.__main__:main'],
+        'console_scripts': ['firewall-config=firewall.__main__:main'],
     },
 )
